09b8eb5d8faf1d63206340ce7c1fde4f1b02d555
Daniel Gaytan <dnigaytan07@gmail.com>
2020-04-02 14:08:49 -0600

    Price fixed syntax error

--- models/prize.js
+++ models/prize.js
@@ -4,14 +4,15 @@ const PrizeSchema = new mongoose.Schema({
   id: {
     type: mongoose.Schema.Types.ObjectId,
     required: true
-  }
-  pizza_type: { //Hay que revisar como utilizar la API de dominos o cualquier otra API y ver como hacer los pedidos
-      type: Number,
-      required: true
+  },
+  pizza_type: {
+    //Hay que revisar como utilizar la API de dominos o cualquier otra API y ver como hacer los pedidos
+    type: Number,
+    required: true
   },
   price: {
-      type: NumberDecimal(0.0),
-      required: true
+    type: NumberDecimal(0.0),
+    required: true
   }
 });
 
