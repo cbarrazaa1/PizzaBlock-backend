455329cb9ce41fbbf1e42b3cc83845683136bbd7
Daniel Gaytan <dnigaytan07@gmail.com>
2020-04-06 17:37:15 -0500

    Updated server

--- server.js
+++ server.js
@@ -2,23 +2,36 @@ const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
 const morgan = require("morgan");
+const bodyParser = require("body-parser");
 const cors = require("cors");
 const uuid = require("uuid");
 
 const app = express();
+
+let user = require("./routes/user");
+let game = require("./routes/game");
 
 app.use(express.static("public"));
 app.use(morgan("dev"));
 
 const { DATABASE_URL, PORT } = require("./config");
 
+mongoose.set("useNewUrlParser", true);
+mongoose.connect("mongodb://localhost/backend-pizzablock", {
+  useUnifiedTopology: true,
+});
+
 mongoose.Promise = global.Promise;
+
+app.use(bodyParser.json());
+app.use(user);
+app.use(game);
 
 let server;
 
 function runServer(port, DATABASE_URL) {
   return new Promise((resolve, reject) => {
-    mongoose.connect(DATABASE_URL, response => {
+    mongoose.connect(DATABASE_URL, (response) => {
       if (response) {
         return reject(response);
       } else {
@@ -27,7 +40,7 @@ function runServer(port, DATABASE_URL) {
             console.log("App is running on port " + port);
             resolve();
           })
-          .on("error", err => {
+          .on("error", (err) => {
             mongoose.disconnect();
             return reject(err);
           });
@@ -40,7 +53,7 @@ function closeServer() {
   return mongoose.disconnect().then(() => {
     return new Promise((resolve, reject) => {
       console.log("Closing the server");
-      server.close(err => {
+      server.close((err) => {
         if (err) {
           return reject(err);
         } else {
@@ -51,7 +64,7 @@ function closeServer() {
   });
 }
 
-runServer(PORT, DATABASE_URL).catch(err => {
+runServer(PORT, DATABASE_URL).catch((err) => {
   console.log(err);
 });
 
